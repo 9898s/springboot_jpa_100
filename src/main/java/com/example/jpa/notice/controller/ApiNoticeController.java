2b5626d56d8ee05ec4a022e6bd7e5bcbe33e7e74
9898s <sueh9898@icloud.com>
2023-08-18 09:00:35 +0900

    :sparkles: 게시글 기본 목록

---
+++ src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
@@ -0,0 +1,61 @@
+package com.example.jpa.notice.controller;
+
+import com.example.jpa.notice.model.NoticeModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ApiNoticeController {
+
+//    @GetMapping("/api/notice")
+//    public String noticeString() {
+//        return "공지사항입니다.";
+//    }
+
+//    @GetMapping("/api/notice")
+//    public NoticeModel notice() {
+//        LocalDateTime regDate = LocalDateTime.of(2023, 8, 18, 0, 0);
+//
+//        NoticeModel notice = new NoticeModel();
+//        notice.setId(1);
+//        notice.setTitle("공지사항입니다.");
+//        notice.setContents("공지사항 내용입니다.");
+//        notice.setRegDate(regDate);
+//        return notice;
+//    }
+
+//    @GetMapping("/api/notice")
+//    public List<NoticeModel> notice() {
+//        List<NoticeModel> noticeList = new ArrayList<>();
+//
+//        noticeList.add(NoticeModel.builder()
+//                .id(1)
+//                .title("첫번째 공지사항입니다.")
+//                .contents("첫번째 공지사항 내용입니다.")
+//                .regDate(LocalDateTime.now())
+//                .build());
+//
+//        noticeList.add(NoticeModel.builder()
+//                .id(2)
+//                .title("두번째 공지사항입니다.")
+//                .contents("두번째 공지사항 내용입니다.")
+//                .regDate(LocalDateTime.now())
+//                .build());
+//
+//        return noticeList;
+//    }
+
+    @GetMapping("/api/notice")
+    public List<NoticeModel> notice() {
+        List<NoticeModel> noticeList = new ArrayList<>();
+        return noticeList;
+    }
+
+    @GetMapping("/api/notice/count")
+    public int noticeCount() {
+        return 10;
+    }
+}
