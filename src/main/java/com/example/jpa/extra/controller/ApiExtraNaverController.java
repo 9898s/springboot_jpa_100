3cd6923a45b862bca1de5e6e756de8bca9a7c2b8
9898s <sueh9898@icloud.com>
2023-09-01 18:16:39 +0900

    :sparkles: 프로퍼터 활용

--- src/main/java/com/example/jpa/extra/controller/ApiExtraNaverController.java
+++ src/main/java/com/example/jpa/extra/controller/ApiExtraNaverController.java
@@ -1,6 +1,7 @@
 package com.example.jpa.extra.controller;
 
 import com.example.jpa.common.model.ResponseResult;
+import com.example.jpa.common.properties.NaverAppProperties;
 import com.example.jpa.extra.model.NaverTranslateInput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
@@ -17,6 +18,39 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 @RestController
 public class ApiExtraNaverController {
+    private final NaverAppProperties naverAppProperties;
+
+
+//    @GetMapping("/api/extra/naver/translate")
+//    public ResponseEntity<?> translate(@RequestBody NaverTranslateInput naverTranslateInput) {
+////        curl "https://openapi.naver.com/v1/papago/n2mt" \
+////        -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
+////        -H "X-Naver-Client-Id: sqkyEIBHze2J8A54vyGc" \
+////        -H "X-Naver-Client-Secret: 3cmn5jfWpo" \
+////        -d "source=ko&target=en&text=만나서 반갑습니다." -v
+//
+//        String clientId = "sqkyEIBHze2J8A54vyGc";
+//        String clientSecret = "3cmn5jfWpo";
+//
+//        String url = "https://openapi.naver.com/v1/papago/n2mt";
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//        headers.add("X-Naver-Client-Id", clientId);
+//        headers.add("X-Naver-Client-Secret", clientSecret);
+//
+//        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+//        requestBody.add("source", "ko");
+//        requestBody.add("target", "en");
+//        requestBody.add("text", naverTranslateInput.getText());
+//
+//        HttpEntity requestEntity = new HttpEntity<>(requestBody, headers);
+//        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, requestEntity, String.class);
+//
+//        return ResponseResult.success(responseEntity.getBody());
+//    }
 
     @GetMapping("/api/extra/naver/translate")
     public ResponseEntity<?> translate(@RequestBody NaverTranslateInput naverTranslateInput) {
@@ -26,8 +60,8 @@ public class ApiExtraNaverController {
 //        -H "X-Naver-Client-Secret: 3cmn5jfWpo" \
 //        -d "source=ko&target=en&text=만나서 반갑습니다." -v
 
-        String clientId = "sqkyEIBHze2J8A54vyGc";
-        String clientSecret = "3cmn5jfWpo";
+        String clientId = naverAppProperties.getClientId();
+        String clientSecret = naverAppProperties.getClientSecret();
 
         String url = "https://openapi.naver.com/v1/papago/n2mt";
 
